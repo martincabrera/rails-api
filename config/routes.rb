d1c1dd94ff8e63541ce430499fdec0059784534b
Martin Cabrera <martin@kisland.com>
2015-04-14 22:44:38 +0200

    Spacing between brackets

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
     # It's good to have versioning in place even though there's only one right now :)
-    scope module: "v1" ,as: "v1", defaults: { format: :json}, except: [:new, :edit] do
+    scope module: "v1" ,as: "v1", defaults: { format: :json }, except: [:new, :edit] do
       resources :courses do
         resources :assignments
       end
